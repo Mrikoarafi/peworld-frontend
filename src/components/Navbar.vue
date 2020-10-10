29224960d5b85a676aba5ead2176e760260a995d
wisnu345 <wisnugiri64@gmail.com>
2020-10-10 14:01:56 +0700

    check conflicts

---
+++ src/components/Navbar.vue
@@ -0,0 +1,194 @@
+<template>
+  <div>
+    <b-navbar toggleable="md" type="light" class=" h-158 mt-10">
+      <b-navbar-brand href="" class=" d-flex" @click="home">
+        <img src="../assets/icons/Peworld Logo.svg" alt="">
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"
+        ><img src="../assets/icons/align-right.png"
+      /></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav class="ml-sm-auto mt-sm-3">
+        <b-navbar-nav class=" ml-auto" v-if="logged">
+          <b-navbar-nav class=" mr-xl-auto notif-chat">
+            <b-nav-item href="#" class="d-block d-md-none">Chats</b-nav-item>
+            <b-nav-item href="#" class="d-block d-md-none"
+              >Notifications</b-nav-item
+            >
+            <b-nav-item href="#" class="d-none d-md-flex"
+              ><img src="../assets/icons/mail.png"
+            /></b-nav-item>
+            <b-nav-item href="/notifications" class="d-none d-md-flex"
+              ><img src="../assets/icons/bell.png"
+            /></b-nav-item>
+          </b-navbar-nav>
+          <b-nav-item-dropdown
+            toggle-class=" text-decoration-none mr-5"
+            no-caret
+            right
+          >
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <div class=" borderprofile rounded-circle ">
+                <img
+                  class="rounded-circle"
+                  :src="`${url}/${getdetaildata.image}`"
+                  alt=""
+                />
+              </div>
+            </template>
+            <b-dropdown-item to="/profile">Profile</b-dropdown-item>
+            <b-dropdown-item @click="logout">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto" v-else>
+          <router-link
+            to="/register"
+            type="button"
+            class="btn mr-2 btn-outline-signup"
+            >Masuk</router-link
+          >
+          <router-link
+            to="/register"
+            type="button"
+            class="btn btnsignup"
+            >Daftar</router-link
+          >
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+// import { mapActions, mapGetters } from 'vuex'
+// const { url } = require('../helper/env')
+
+export default {
+  data () {
+    return {
+      logged: true,
+      dataUser: {}
+    }
+  },
+  methods: {
+    logout () {
+      localStorage.removeItem('token')
+      localStorage.removeItem('refreshToken')
+      localStorage.removeItem('id')
+      window.location.reload()
+    },
+    // ...mapActions({
+    //   getUser: 'user/getUserDetail',
+    //   onUpdateData: 'user/updateData'
+    // }),
+    home () {
+      window.location = '/'
+    }
+  }
+  // computed: {
+  //   ...mapGetters({
+  //     getdetaildata: 'user/getallData'
+  //   })
+  // }
+}
+</script>
+
+<style scoped>
+.bg-lighto {
+  background: #fff;
+}
+.navbar-light .navbar-toggler {
+  color: rgba(0, 0, 0, 0.5);
+  border: none;
+}
+/* .navbar {
+    padding: 0.5rem 1rem;
+    margin: 10px 0px;
+} */
+.divsearch {
+  background: #f5f5f5;
+  color: #6b6b6b;
+  font-family: Lato;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+}
+.divsearch input {
+  background: rgba(0, 0, 0, 0);
+  border: none;
+}
+.notif-chat {
+  width: 80px;
+}
+.notif-chat img {
+  width: 100%;
+}
+.notif-chat li.nav-item {
+  display: flex;
+  align-items: center;
+}
+.borderprofile {
+  border: 2px #00b7df solid;
+  width: 50px;
+  height: 50px;
+  padding: 2px;
+}
+.borderprofile img {
+  width: 100%;
+  height: 100%;
+}
+.navlink.dropdown-toggle::after {
+  display: none;
+  margin-left: 0.255em;
+  vertical-align: 0.255em;
+  content: "";
+  border-top: 0.3em solid;
+  border-right: 0.3em solid transparent;
+  border-bottom: 0;
+  border-left: 0.3em solid transparent;
+}
+.navigation {
+  color: #000;
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  margin: 0px 10px;
+}
+.btn {
+  width: 100%;
+  font-size: 15px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 700;
+  margin: 10px 0px;
+  border-radius: 10px;
+}
+.btnsignup {
+  background: #5E50A1;
+  border-radius: 4px;
+  border: none;
+  color: white;
+}
+.btn-outline-signup {
+  border: 2px #5E50A1 solid;
+  border-radius: 4px;
+  background: none;
+  color: #5E50A1;
+}
+@media (max-width: 767px) {
+  .active::after {
+    display: none;
+  }
+  #nav-collapse {
+    background-color: white;
+    padding: 30px;
+    border-radius: 0px 0px 10px 10px;
+  }
+  .borderprofile {
+    border: 2px #fff solid;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import axios from 'axios'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// const { url } = require('./helper/env')
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+// axios.defaults.headers = {
+//   token: localStorage.getItem('token')
+// }
 
 new Vue({
   router,
