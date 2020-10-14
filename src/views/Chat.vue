88296f53a8076e27ef27d91d6079f118c21be6a5
yyy-pixel <pnksm009@gmail.com>
2020-10-14 22:44:37 +0700

    add chat

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -7,7 +7,7 @@
         <div class="col-sm-12 d-flex heightChat">
           <div class="col-sm-4 form-login mt-4">
             <div class="row listUser pl-0 pr-0">
-              <div class="col-12 pl-0 pr-0">
+              <div class="col-12 pl-0 pr-0 mt-4">
                 <small class="mx-4 " style="font-size: 16px; font-weight: 600">
                   Chat
                 </small>
@@ -16,13 +16,20 @@
 
               <div
                 class="col-12 my-auto mx-auto text-center"
-                v-if="chat.length === 0"
+                v-if="listCalling[0].length === 0"
               >
                 <img src="..\assets\images\Chat\chat 0.png" />
               </div>
-              <div class="col-12 my-auto mx-auto text-center" v-else>
-                <div v-for="(item, index) in chat" :key="index">
-                  Chat looping
+
+              <div class="col-12 mt-4 mx-auto text-center" v-else>
+                <div class="btn btn-primary" v-for="(item, index) in listCalling[0]" :key="index">
+                  <div v-if="role == 0" @click="chats(item.email_recruiter)">
+                    {{item.email_recruiter}}
+                  </div>
+
+                  <div v-else @click="chats(item.email_employe)">
+                    {{item.email_employe}}
+                  </div>
                 </div>
               </div>
             </div>
@@ -36,24 +43,38 @@
                 </small>
                 <div class="mt-3" style="border: 1px solid #e2e5ed"></div>
               </div>
-              <div class="col-12 p-0 listMessage">
-                <div class="row mx-4 mt-4">Udin gele</div>
+              <div class="ml-4 d-inline" v-for="(item, index) in historyMsg" :key="index">
+                {{item.sender}}<div v-html="item.message"></div>
+              </div>
+              <div class="col-12 p-0 listMessage" v-if="receiver !== null">
+                <div v-for="(item, index) in getAllPrivateMessage" :key="index">
+                  {{item.message}}
+                </div>
+              </div>
+
+              <div v-else>
+                <div class="row mt-4" v-for="(item, index) in listMsg" :key="index">
+                  {{item.message}}
+                </div>
               </div>
               <div class="col-12 p-0 mt-auto mb-4">
                 <div class="row mx-4">
-                  <input
+                  <form @submit.prevent="send">
+                    <input
                     type="text"
                     class="form-control col-11"
                     placeholder="type message..."
                     style="border-radius: 30px"
+                    v-model="message"
                   />
                   <button
+                  type="submit"
                     style="background: #5e50a1"
                     class="ml-auto btn btn text-white rounded-circle py-2"
-                    @click.prevent="addSkill"
                   >
                     <img src="..\assets\images\Chat\send.png" alt="" />
                   </button>
+                  </form>
                 </div>
               </div>
             </div>
@@ -68,16 +89,102 @@
 <script>
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
+import io from 'socket.io-client'
 
 export default {
   data () {
     return {
-      chat: []
+      chat: [],
+      socket: io('http://localhost:3000'),
+      listCalling: [],
+      email: localStorage.getItem('email'),
+      role: localStorage.getItem('role'),
+      message: '',
+      receiver: null,
+      listMsg: [],
+      getAllPrivateMessage: [],
+      historyMsg: []
     }
   },
   components: {
     Navbar,
     Footer
+  },
+  methods: {
+    chats (emailReceiver) {
+      this.listMsg = []
+      this.getAllPrivateMessage = []
+      this.receiver = emailReceiver
+      this.getPrivateMessage()
+      this.socket.emit('get-all-message', {
+        sender: this.email,
+        receiver: emailReceiver
+      })
+
+      this.getHistoryMsg()
+    },
+    send () {
+      if (!this.receiver) {
+        alert('receiver is null')
+      } else {
+        const msg = `${this.email} : ${this.message}`
+
+        this.listMsg = [...this.listMsg, {
+          sender: this.email,
+          receiver: this.receiver,
+          message: msg
+        }]
+
+        this.getPrivateMessage()
+
+        this.socket.emit('send-message', {
+          sender: this.email,
+          receiver: this.receiver,
+          message: this.message
+        })
+
+        this.message = ''
+      }
+    },
+    getPrivateMessage () {
+      const privateMsg = this.listMsg.filter(e => {
+        if (this.receiver === null) {
+          return e.sender === this.receiver || e.sender === this.email
+        } else {
+          return e.receiver === this.receiver || e.sender === this.receiver
+        }
+      })
+
+      this.getAllPrivateMessage = privateMsg
+    },
+    getHistoryMsg () {
+      this.socket.on('get-history-message', payload => {
+        this.historyMsg = payload
+        console.log(payload)
+      })
+    }
+  },
+  mounted () {
+    // this.socket.on('list-calling', (payload) => {
+    //   console.log(payload)
+    // })
+
+    this.getHistoryMsg()
+    this.socket.emit('get-all-calling', (this.email))
+
+    this.socket.on('list-calling', (payload) => {
+      this.listCalling = [...this.listCalling, payload]
+    })
+
+    this.socket.emit('join-room', this.email)
+
+    this.socket.on('get-message', payload => {
+      this.listMsg = [...this.listMsg, payload]
+
+      if (this.receiver !== null) {
+        this.getPrivateMessage()
+      }
+    })
   }
 }
 </script>
@@ -96,7 +203,7 @@ export default {
   border-radius: 13px;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  /* align-items: center; */
 }
 .listMessage {
   min-height: 440px;
--- src/views/JobHire.vue
+++ src/views/JobHire.vue
@@ -207,3 +207,177 @@ textarea:focus {
   }
 }
 </style>
+
+<!-- <template>
+  <div class="job-hire">
+    <Navbar type="home" />
+    <img src="../assets/images/logo.png" alt="">
+    <div class="container mt-4 mb-5">
+      <div class="row">
+        <div class="col-sm-4 mb-5">
+          <div class="card-left p-4">
+            <div class=" text-center mb-3">
+              <img src="../assets/images/profile.png" alt="photo profile">
+            </div>
+            <h5 class="font-weight-bold">{{ detailEmploye.name }}</h5>
+            <p class="small">{{ detailEmploye.jobdesk }}</p>
+            <p class="small text-muted">
+              <img src="../assets/images/map.png" alt="location" class="location mr-1">
+              {{ detailEmploye.domisili }}
+            </p>
+            <p class="small text-muted">
+              {{ detailEmploye.description }}
+            </p>
+            <h5 class="font-weight-bold">Skill</h5>
+            <a class="btn btn-skill text-white mr-2 mt-2 mb-2">PHP</a>
+          </div>
+        </div>
+
+        <div class="col-sm-8">
+          <div class="card-right">
+            <h3 class="font-weight-bold">Hubungi Louis Tomlinson</h3>
+            <p class="small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.</p>
+            <form @submit.prevent="sendHire()">
+              <label class="small text-muted mt-3">Tujuan tentang pesan ini</label>
+              <select class="form-control">
+                <option>Projek</option>
+              </select>
+              <label class="small text-muted mt-3">Nama Lengkap</label>
+              <input type="text" class="form-control" placeholder="Masukkan nama lengkap">
+              <label class="small text-muted mt-3">Email</label>
+              <input type="text" class="form-control" placeholder="Masukkan email">
+              <label class="small text-muted mt-3">No Handphone</label>
+              <input type="text" class="form-control" placeholder="Masukkan no handphone">
+              <label class="small text-muted mt-3">Deskripsi</label>
+              <textarea class="form-control" rows="8" placeholder="Deskripsi / jelaskan lebih detail"></textarea>
+              <button type="submit" class="btn btn-orange btn-block text-white mt-5">
+                Hire
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+import io from 'socket.io-client'
+
+export default {
+  name: 'JobHire',
+  data () {
+    return {
+      id: localStorage.getItem('id'),
+      socket: io('http://localhost:3000'),
+      email: localStorage.getItem('email'),
+      idCompany: localStorage.getItem('id_company'),
+      image: ''
+    }
+  },
+  components: {
+    Navbar,
+    Footer
+  },
+  computed: {
+    ...mapGetters({
+      detailEmploye: 'employe/getDetail',
+      detailRecruiter: 'company/getDetail',
+      detailCompany: 'company/getDetailCompany'
+    })
+  },
+  methods: {
+    ...mapActions({
+      onDetail: 'company/onDetail',
+      onDetailCompany: 'company/onDetailCompany'
+    }),
+    sendHire () {
+      this.socket.emit('send-hire-calling', {
+        id_company: this.idCompany,
+        email_recruiter: this.email,
+        email_employe: 'satusebelas0@gmail.com'
+      })
+
+      this.socket.emit('send-hire-message', {
+        sender: this.email,
+        receiver: 'satusebelas0@gmail.com',
+        message: `<div class="card mb-3 mt-3" style="width: 35rem;">
+                    <img src="${this.detailCompany.image_company}" class="card-img-top" style="height: 20%;">
+                    <div class="card-body">
+                      <div class="list-group">
+                        <div class="list-group-item text-center"><h5 class="card-title">${this.detailRecruiter.company_name}</h5></div>
+                        <div class="list-group-item text-left"><p class="card-text">to: satusebelas0@gmail.com</p></div>
+                        <div class="list-group-item text-left"><p class="card-text">Job Desk: Pembuatan Aplikasi Ekopoi</p></div>
+                        <div class="list-group-item"><p class="card-text">Membuat aplikasi Ekopoi menggunakan VueJS dan NodeJS</p></div>
+                        <div class="list-group-item"><p class="card-text"><small class="text-muted">Contact Us: ${this.detailCompany.phone_number}</small></p></div>
+                      </div>
+
+                      <div class="text-center">
+                        <button class="btn btn-primary btn-sm mt-3">Apply</button>
+                      </div>
+                    </div>
+                  </div>
+                `
+      })
+
+      window.location = '/chat'
+    }
+  },
+  mounted () {
+    this.onDetail(this.id)
+    this.onDetailCompany(this.idCompany)
+
+    this.socket.emit('join-room', this.email)
+  }
+}
+</script>
+
+<style scoped>
+.job-hire {
+  background-color: #E5E5E5;
+  overflow: hidden;
+}
+.card-left {
+  background-color: #fff;
+  border-radius: 8px;
+}
+.location {
+  margin-top: -5px;
+}
+.btn-skill {
+  background: rgba(251, 176, 23, 0.6);
+  border: 1px solid #FBB017;
+  box-sizing: border-box;
+  border-radius: 4px;
+  padding: 3px 15px;
+  font-size: 12px;
+  margin-top: -5px;
+}
+select,
+input[type="text"],
+input[type="email"],
+textarea {
+  background: #fff;
+  border: none;
+  -webkit-box-shadow: none;
+  box-shadow: none;
+  font-size: 14px;
+}
+select:focus,
+input[type="text"]:focus,
+input[type="email"]:focus,
+textarea:focus {
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
+.btn-orange {
+  background-color: #FBB017;
+  border-radius: 5px;
+  font-weight: bold;
+}
+</style>
+-->
